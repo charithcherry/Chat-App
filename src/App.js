38baff79f5134742e9c864ba624222b592ae5187
charith <charithcherry1100gmail.com>
2021-02-02 20:06:36 +0530

    Phase 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>
+{
+ return(
+     <ChatEngine
+     height="100vh"
+     projectID="0ae31c43-5dfb-4cf8-a5de-a9599106d1c4"
+     userName="jmaster"
+     userSecret="123123"
+     renderChatFeed={ (chatAppprops)=> <ChatFeed {...chatAppprops}/>}
+     
+     />
+ );
+
 }
-
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,13 @@
+const ChatFeed=(props)=>
+{
+
+console.log(props);
+return(
+
+    <div>
+        ChatFeed
+    </div>
+);
+
+}
+export default ChatFeed;
