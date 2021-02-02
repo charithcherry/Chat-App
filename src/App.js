88cf26a6eba51327eaf4074abda6a2784962752f
charith <charithcherry1100gmail.com>
2021-02-02 23:24:19 +0530

    phase 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import Modal from './components/LoginForm'
 
 
 const App=()=>
 {
+
+   if(!localStorage.getItem('username')) return <Modal/>
  return(
      <ChatEngine
      height="100vh"
      projectID="0ae31c43-5dfb-4cf8-a5de-a9599106d1c4"
-     userName="jmaster"
-     userSecret="123123"
-     renderChatFeed={ (chatAppprops)=> <ChatFeed {...chatAppprops}/>}
+     userName={localStorage.getItem('username')}
+     userSecret={localStorage.getItem('password')}
+     renderChatFeed={ (chatAppProps)=> <ChatFeed {...chatAppProps}/>}
      
      />
  );
